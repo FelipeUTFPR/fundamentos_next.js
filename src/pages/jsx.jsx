941dfca46257c49fb29c7e55ccd4bd4044b35f4b
FelipeUTFPR <fps2887@gmail.com>
2022-02-21 09:18:54 -0300

    86. Integração JavaScript/JSX]

---
+++ src/pages/jsx.jsx
@@ -0,0 +1,21 @@
+export default function Jsx(){
+    //Nesse espaço posso colocar pedaços de javascript
+    const titulo = <h1> JSX é um Conceito Central</h1>
+
+function subtitulo(){
+    return <h2>{"muito legal".toLocaleUpperCase()}</h2>
+
+}
+
+return(
+    <div>
+    {/* posso mesclar javascript dentro de jsx utilizando chaves */}
+        {titulo}
+        {subtitulo()}
+    <p>
+        {JSON.stringify({nome: 'João', idade:30})}
+    </p>
+    </div>
+)
+
+}
