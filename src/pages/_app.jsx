f639eec02cccecc882c10d41b14959f97c638ce0
FelipeUTFPR <fps2887@gmail.com>
2022-02-21 09:27:20 -0300

    87. CSS Global

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,6 @@
+//aqui os css globais podem ser referenciados, eles afetam todo o nosso programa
 import '../styles/globals.css'
+import '../styles/app.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
