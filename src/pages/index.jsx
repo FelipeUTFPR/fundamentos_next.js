708d07df51e384f61ff958c92037be3313a4c290
FelipeUTFPR <fps2887@gmail.com>
2022-02-21 09:01:31 -0300

    85. Entendendo o JSX

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,12 @@
 export default function Início(){
-  return "Início 2"
+  //A div pode ser colocada na linha do returna,
+  //mas se for colocado abaixo. é necessário colocar entre parenteses
+  //retorna um trecho de jsx
+  return (
+    <div>
+      <h1>Fundamentos de Next.js & React</h1>
+      <h2>Vamos estudar esse framework</h2>
+    </div>
+
+  )
 }
