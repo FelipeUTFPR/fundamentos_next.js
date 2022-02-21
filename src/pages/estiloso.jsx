1a92b776dec61c3fefddb9719c2fd455135d2bc9
FelipeUTFPR <fps2887@gmail.com>
2022-02-21 10:59:58 -0300

    90. Navegação entre Componentes

--- src/pages/estiloso.jsx
+++ src/pages/estiloso.jsx
@@ -1,8 +1,11 @@
 import styles from '../styles/Estiloso.module.css'
+import Link from 'next/link'
 
 export default function Estiloso(){
     return(
         <div className={styles.roxo}>
+        {/* Cria um link de retorno par ao index */}
+        <Link href="/">voltar</Link>
         <h1>Estilo usando CSS Módulos</h1>
 
         </div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,13 @@
+import Link from 'next/link'
+
 export default function Início(){
-  //A div pode ser colocada na linha do returna,
-  //mas se for colocado abaixo. é necessário colocar entre parenteses
-  //retorna um trecho de jsx
+  
   return (
     <div>
-      <h1>Fundamentos de Next.js & React</h1>
-      <h2>Vamos estudar esse framework</h2>
+    {/* cria um link de navegação para a página estiloso */}
+      <Link href="/estiloso">
+        Estilo
+      </Link>
     </div>
 
   )
