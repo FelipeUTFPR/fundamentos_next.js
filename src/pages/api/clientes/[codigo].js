40f6e04ef4bf6b20ee936e5c1dc0fd3415f6b21d
FelipeUTFPR <fps2887@gmail.com>
2022-02-25 10:36:37 -0300

    98. Usando API #02

---
+++ src/pages/api/clientes/[codigo].js
@@ -0,0 +1,10 @@
+//Acesso pelo link http://localhost:3000/api/clientes/123
+
+export default function handler(req,res){
+    const codigo = req.query.codigo
+    res.status(2000).json({
+        id:codigo,
+        nome: `Maria ${codigo}`,
+        email: `mariamariamaria${codigo}@xcfmail.com`
+    })
+}
