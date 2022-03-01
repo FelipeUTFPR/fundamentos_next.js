71044446f6637328e10569a8bf6151d95da30e2f
FelipeUTFPR <fps2887@gmail.com>
2022-03-01 11:11:35 -0300

    101. Modelos de Pré-Renderização

--- src/components/Navegador.jsx
+++ src/components/Navegador.jsx
@@ -3,7 +3,8 @@ import Link from "next/link"
 
 export default function Navegador(props){
     return(
-        <Link href={props.destino}>
+        //passHref para retirar o Warning
+        <Link href={props.destino} passHref>
         {/* aplicando um estilo de forma dinâmica */}
         <div className={styles.navegador} style={{
             backgroundColor: props.cor ?? 'dodgerblue'
